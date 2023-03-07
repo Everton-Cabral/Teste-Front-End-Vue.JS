ba60c86377e93189118e5c3e7925598916100b81
Everton Cabral <90525060+Everton-Cabral@users.noreply.github.com>
2023-03-07 11:15:46 -0300

    criando o componente alerta e suas funcionalidades

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,5 @@ export default defineComponent({
   margin: 0px;
   padding: 0px;
 }
+
 </style>
--- src/store.js
+++ src/store.js
@@ -2,14 +2,16 @@ import { createStore } from 'vuex'
 export const store = createStore({
     state(){
         return{
-            userSearch: false
+            userSearch: false,
+            alert: false
         }
     },
     mutations:{
-   
-            changeSearch(state){
-                state.userSearch = !state.userSearch
-            
+        changeSearch(state){
+            state.userSearch = !state.userSearch  
+        },
+        closeAlert(state){
+            state.alert = false
         }
     }
 })
